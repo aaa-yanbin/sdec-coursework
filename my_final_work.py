08b11c4ecd429f170fe5843e64677cc5521547cc
aaa-yanbin <94594122+aaa-yanbin@users.noreply.github.com>
2022-05-17 19:43:26 +0800

    Update my_final_work.py

--- my_final_work.py
+++ my_final_work.py
@@ -40,8 +40,8 @@ prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
 # st.write(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'])
-iris_list = [[0,'Iris-setosa'],[1,'Iris-versicolor'],[2,'Iris-virginica']]
-df = pd.DataFrame (iris_list, columns = ['Class_Labels', 'Iris_Type'])
+iris_list = [['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']]
+df = pd.DataFrame (iris_list)
 st.dataframe(df)
 
 st.subheader('Prediction')
